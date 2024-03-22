05d4fab214cc86547075c01ee19fb810b9ddd925
Soltyssss08 <263495@student.pwr.edu.pl>
2024-03-22 17:04:50 +0100

    Color type

--- read_metadata.py
+++ read_metadata.py
@@ -15,7 +15,7 @@ def read_png_metadata(file_path):
     with open(file_path, 'rb') as file:
         header = file.read(8)
         if header[:8] != b'\x89PNG\r\n\x1a\n':
-            raise ValueError("To nie jest plik PNG")
+            raise ValueError("This is not PNG flie")
 
         metadata = {}
 
@@ -26,23 +26,35 @@ def read_png_metadata(file_path):
             length = int.from_bytes(length_bytes, byteorder='big')
 
             block_type = file.read(4)
-
             data = file.read(length)
-
             crc = file.read(4)
 
             if block_type == b'IHDR':
                 metadata = read_IHDR(data)
 
         return metadata
+    
+def recoginze_color_type(number):
+    match number:
+        case 0:
+            return "Color, palette and alpha channel not used"
+        case 2:
+            return "Color used"
+        case 3:
+            return "Color and palette used"
+        case 4:
+            return "Alpha channel used"
+        case 6:
+            return "Color, palette and alpha channel used"
+        case _:
+            return "Wrong number"
 
-# Przykładowe użycie
-file_path = 'example.png'
+file_path = 'example2.png'
 metadata = read_png_metadata(file_path)
 print("Width:", metadata.get('width'))
 print("Height:", metadata.get('height'))
 print("Bit depth:", metadata.get('bit_depth'))
-print("Color type:", metadata.get('color_type'))
+print("Color type:", metadata.get('color_type'),", ", recoginze_color_type(metadata.get('color_type')))
 print("Compression method:", metadata.get('compression_method'))
 print("Filter method:", metadata.get('filter_method'))
 print("Interlace method:", metadata.get('interlace_method'))
