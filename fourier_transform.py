6ad3db8fceb1dec513c8f94c37c02c953bc137f4
269460 <134083681+269460@users.noreply.github.com>
2024-04-12 20:02:43 +0200

    Update fourier_transform.py

--- fourier_transform.py
+++ fourier_transform.py
@@ -1,22 +1,19 @@
+
 import cv2
 import numpy as np
 
-image = cv2.imread(r"example4.png")
+#image = cv2.imread(r"example4.png")
+def fourier_transform_image(file_path):
+    image = cv2.imread(file_path)
+    if image is None:
+        raise ValueError("Nie można załadować obrazu.")
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-fourier = cv2.dft(np.float32(gray), flags=cv2.DFT_COMPLEX_OUTPUT)
-
-fourier_shift = np.fft.fftshift(fourier)
-
-magnitude = 20 * np.log(cv2.magnitude(fourier_shift[:, :, 0], fourier_shift[:, :, 1]))
-phase = np.angle(fourier_shift[:, :, 0] + 1j * fourier_shift[:, :, 1])
-
-magnitude = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-
-cv2.imshow('Original Image', gray)
-cv2.imshow('Fourier Transform Magnitude', magnitude)
-cv2.imshow('Fourier Transform Phase', phase)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    fourier = cv2.dft(np.float32(gray), flags=cv2.DFT_COMPLEX_OUTPUT)
+    fourier_shift = np.fft.fftshift(fourier)
+    magnitude = 20 * np.log(cv2.magnitude(fourier_shift[:,:,0], fourier_shift[:,:,1]))
+    magnitude = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    cv2.imshow('Fourier Transform', magnitude)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
